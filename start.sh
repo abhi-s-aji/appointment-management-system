3c3adf17c73b85138cb986df1d9501abdc75db90
abhi-s-aji <abhisajieden@gmail.com>
2026-06-06 04:28:52 +0530

    Add missing start.sh script

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+python manage.py shell -c "from django.contrib.sites.models import Site; from allauth.socialaccount.models import SocialApp; import os; site, _ = Site.objects.update_or_create(id=1, defaults={'domain': 'appointment-management-system-production-abhisaji.up.railway.app', 'name': 'Appointment System'}); app, _ = SocialApp.objects.update_or_create(provider='google', defaults={'name': 'Google', 'client_id': os.environ.get('GOOGLE_CLIENT_ID'), 'secret': os.environ.get('GOOGLE_CLIENT_SECRET')}); app.sites.add(site)"
+gunicorn django_skills.wsgi:application --bind 0.0.0.0:$PORT
