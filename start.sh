ed9b62db289374a09dc5ec050240f5cf6f8883bb
abhi-s-aji <abhisajieden@gmail.com>
2026-06-06 04:36:41 +0530

    Add check for environment variables

--- start.sh
+++ start.sh
@@ -4,11 +4,16 @@ python manage.py collectstatic --noinput
 
 python manage.py shell -c "
 import os
+import sys
 from django.contrib.sites.models import Site
 from allauth.socialaccount.models import SocialApp
 
-# Debug: Print the environment variables to the logs
-print(f'DEBUG: GOOGLE_CLIENT_ID is {os.environ.get('GOOGLE_CLIENT_ID')}')
+client_id = os.environ.get('GOOGLE_CLIENT_ID')
+secret = os.environ.get('GOOGLE_CLIENT_SECRET')
+
+if not client_id or not secret:
+    print('ERROR: Environment variables GOOGLE_CLIENT_ID or GOOGLE_CLIENT_SECRET are missing!')
+    sys.exit(1)
 
 site, _ = Site.objects.update_or_create(
     id=1, 
@@ -19,8 +24,8 @@ app, _ = SocialApp.objects.update_or_create(
     provider='google',
     defaults={
         'name': 'Google',
-        'client_id': os.environ.get('GOOGLE_CLIENT_ID'),
-        'secret': os.environ.get('GOOGLE_CLIENT_SECRET')
+        'client_id': client_id,
+        'secret': secret
     }
 )
 app.sites.add(site)
