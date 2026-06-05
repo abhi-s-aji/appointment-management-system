d07768a076af87c552701dd09cb02174c7c93406
abhi-s-aji <abhisajieden@gmail.com>
2026-06-06 03:42:00 +0530

    Enable DEBUG mode for debugging

--- django_skills/settings.py
+++ django_skills/settings.py
@@ -5,7 +5,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['appointment-management-system-production-abhisaji.up.railway.app']
 
 INSTALLED_APPS = [
